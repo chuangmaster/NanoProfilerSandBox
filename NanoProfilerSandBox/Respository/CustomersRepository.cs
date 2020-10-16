bb73c2cd92e3976801402e1ce15e5f62c0ac2dc7
chuangmaster <masterchuang@urtody.com>
2020-10-16 11:32:23 +0800

    add repository and use it in customer api

---
+++ NanoProfilerSandBox/Respository/CustomersRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NanoProfilerSandBox.Respository
+{
+    public class CustomersRepository
+    {
+        private NorthwindEntities _DB;
+
+        public CustomersRepository()
+        {
+            _DB = new NorthwindEntities();
+        }
+
+        public List<string> GetCustomerContactName()
+        {
+            return _DB.Customers.Select(x => x.ContactName).Take(20).ToList();
+        }
+    }
+}
